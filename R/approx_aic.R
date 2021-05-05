3c790852a1ad2cc957f0431dc67f457597fbc63f
sevag <s.d.kevork@gmail.com>
2021-05-05 11:49:01 +0200

    Initial commit

---
+++ R/approx_aic.R
@@ -0,0 +1,166 @@
+#' Approximate AIC Calculation
+#'
+#' Function to calculate an approximate AIC value for both mERGM and ERGM.
+#' \code{\link[mergm]} and  ERGM \code{\link[ergm]} for model comparison.
+#'
+#' @param model object; generated from \code{\link[mergm]{mergm}}, or
+#' generated from \code{\link[ergm]{ergm}}.
+#'
+#' @param nsim count; number of network simulations needed for the calculation
+#' of the approx. AIC value. Default = 1000
+#'
+#' @details This function returns an approx. AIC value which can be used for
+#' model comparison.
+#'
+#' @seealso \code{\link[mergm]{mergm}}.
+#' @seealso \code{\link[mergm]{convergence}}.
+#'
+#' @examples
+#' \dontrun{
+#' # Load the karate club social network of Zachary (1977)
+#' data("zach", package = "ergm.count")
+#' # Seed for reproducity
+#' set.seed(2410)
+#' # Fit a model to measur the propensity to form edges, 2-stars and
+#' # capture the unobserved nodal heterogeneity, allowing 10 iteration steps.
+#' mod <- mergm(zach ~ edges + kstar(2), 10)
+#' # Get the AIC value
+#' approx_aic(mod)
+#' }
+#'
+#' @import matrixStats
+#'
+#' @export approx_aic
+#'
+approx_aic <- function(model,
+                       nsim = 1000){
+
+  if(!is.ergm(model)){
+    # get the network from the model
+    net <- network(model$nnodes, directed = FALSE)
+    net.formula <- as.formula(paste("~", paste(attr(terms.formula(model$formula),
+                                                    "term.labels"),collapse = " + "),
+                                    " + sociality(nodes = 1:model$nnodes)",
+                                    collapse = " "))
+
+    # get the estimated coefficients of the model to simulate networks
+    coefs <- c(model$coefs, model$random.effects)
+
+    sim_stats <- list()
+    set.seed(2410)
+    sim_stats <- simulate(net.formula, nsim = nsim,
+                          coef = coefs,
+                          basis = net,
+                          control = control.simulate(MCMC.burnin=100000,
+                                                     MCMC.interval=10000))
+
+    sim_stats2 <- attr(sim_stats, "stats")
+
+    ## number of the network statistics included in the model
+    ncoef <- length(model$coefs)
+
+    theta.net.stats <- sum(t(model$coefs) %*% summary(model$formula)[1:ncoef])
+
+    deg.net.stats <- sum(t(model$random.effects) %*% summary(model$formula)[(ncoef+1):(ncol(sim_stats2))])
+
+    modmixed.s.obs <- matrix(nrow = nrow(sim_stats2),
+                             ncol = 1)
+    modmixed.t.obs <- matrix(nrow = nrow(sim_stats2),
+                             ncol = 1)
+
+    for(i in 1:nrow(sim_stats2)){
+      modmixed.s.obs[i,] <- t(model$coefs %*% sim_stats2[i,c(1:ncoef)])
+      modmixed.t.obs[i,] <- t(model$random.effects %*% sim_stats2[i,-c(1:ncoef)])
+    }
+
+    log.kappa.sim <- c()
+    for(i in 1:nrow(sim_stats2)){
+      log.kappa.sim[i] <- logSumExp(modmixed.s.obs[i,]+ modmixed.t.obs[i,])
+    }
+
+    log.kappa.sim <- sum(log.kappa.sim)/nrow(sim_stats2)
+
+    norm.ref <- 0.5* (t(model$random.effects) %*% model$random.effects) * (1/model$var.random.effects)
+    log.var.ref <- 0.5 * model$nnodes * log(model$var.random.effects)
+
+    mean.sim.soc <- c()
+    for(i in 1:ncol(sim_stats2)){
+      mean.sim.soc[i] <- mean(sim_stats2[,i])
+    }
+    mean.sim.soc <- mean(mean.sim.soc[-c(1:ncoef)])
+
+    diff.soc.mean <- matrix(nrow = ncol(sim_stats2[,-c(1:ncoef)]),
+                            ncol = nrow(sim_stats2))
+    prod.diff.soc.mean <- matrix(nrow = ncol(sim_stats2[,-c(1:ncoef)]),
+                                 ncol = length(mean.sim.soc))
+    for(i in 1:nrow(sim_stats2)){
+      diff.soc.mean[,i] <- sim_stats2[i,-c(1:ncoef)] - mean.sim.soc
+      prod.diff.soc.mean <- diff.soc.mean %*% t(diff.soc.mean)
+    }
+    var.soc <- prod.diff.soc.mean / nrow(sim_stats2)
+    var.deg.sim <- var.soc + (1/model$var.random.effects * diag(model$nnodes))
+    var.det <- determinant(var.deg.sim, logarithm = TRUE)
+    var.det <- 2 * var.det$modulus[1]
+
+    const <- 0.5 * model$nnodes * log(2 * pi)
+
+    log.lik.sim <- theta.net.stats + deg.net.stats - log.kappa.sim - norm.ref - log.var.ref - var.det - const
+
+    aic.mergm.mix <- -2 * (log.lik.sim) + 2 * (ncoef + 1)
+
+    out = list(nsim = nsim,
+               mergmAIC = aic.mergm.mix)
+
+    return(out)
+
+  } else{
+
+    net <- network(model$network$gal$n, directed = FALSE)
+    # get the formaula needed
+    net.formula <- as.formula(paste("~", paste(attr(terms.formula(model$formula),
+                                                    "term.labels"),collapse = " + "),
+                                    collapse = " "))
+
+    coefs <- model$coef
+
+    sim_stats <- list()
+    set.seed(2410)
+    sim_stats <- simulate(net.formula, nsim = nsim,
+                          coef = coefs,
+                          basis = net,
+                          control = control.simulate(MCMC.burnin=100000,
+                                                     MCMC.interval=10000))
+
+    sim_stats2 <- attr(sim_stats, "stats")
+    var.ergm <- matrix(nrow=nrow(sim_stats2),ncol=length(coefs))
+    for(i in 1:nrow(sim_stats2)){
+      var.ergm[i,] <- 1/nrow(sim_stats2)*(sum(sim_stats2[i,] - colMeans(sim_stats2)) * t(sim_stats2[i,] - colMeans(sim_stats2)))
+    }
+
+    varcov.ergm.sim <- cov(var.ergm)
+
+    net.stats <- summary(model$formula)
+
+    require("matrixStats")
+    log.kappa.sim <- matrix(nrow=nrow(sim_stats2),ncol=length(coefs))
+    log.kappa.sim <- c()
+    for(i in 1:nrow(sim_stats2)){
+      log.kappa.sim[i] <- logSumExp(t(coefs) * sim_stats2[i,])
+    }
+
+    log.kappa.sim <- sum(log.kappa.sim)/nrow(sim_stats2)
+
+    theta.net.stats <- sum(coefs * net.stats)
+
+    log.lik.sim <- theta.net.stats - log.kappa.sim
+
+    aic.ergm <- -2 * (log.lik.sim) + 2 * (length(model$coef) + 1)
+
+    out = list(nsim = nsim,
+               ergmAIC = aic.ergm)
+
+    return(out)
+  }
+
+}
+
---
+++ R/mergm.R
@@ -0,0 +1,185 @@
+#' Iterative Estimation of Mixed Exponential Random Graph Models with Nodal
+#' Random Effects.
+#'
+#' Function to fit ERGMs with nodal random effects. Estimation is carried out
+#' combining the stepping algorithm (see \code{\link[ergm]{control.ergm}}) for
+#' the fixed parameters and pseudo-likelihood (see \code{\link[mgcv]{bam}})
+#' for the nodal random effects.
+#'
+#' @param formula formula; an \code{R} formula object, of the form
+#' <network> ~ <model terms> where <network> is a \code{\link[network]{network}}
+#' object and <model terms> are \code{\link[ergm]{ergm.terms}}. The term
+#' \code{sociality(1:nodes)} is added automatically to the formula argument.
+#'
+#' @param iter count; number of iterations used for iterative estimation
+#' of the model coefficients and the random effects, 10 iterations are usually
+#' enough.
+#'
+#' @details This implemented algorithm can only handle undirected networks.
+#'
+#' @author
+#' Sevag Kevork
+#'
+#' @references Sevag Kevork, Göran Kaeurmann (2019).
+#' Iterative Estimation of Mixed Exponential Random Graph Models with Nodal
+#' Random Effects.
+#'
+#' @examples
+#' \dontrun{
+#' # Load the karate club social network of Zachary (1977)
+#' data("zach", package = "ergm.count")
+#' # Seed for reproducity
+#' set.seed(2410)
+#' # Fit a model to measur the propensity to form edges, 2-stars and
+#' # capture the unobserved nodal heterogeneity, allowing 10 iteration steps.
+#' mod <- mergm(zach ~ edges + kstar(2), 10)
+#' # GOF of the model
+#' gof(mod$model)
+#' plot(gof(mod$model))
+#' # Summary of the model
+#' summary(mod$model)
+#' }
+#' @import ergm
+#' @import network
+#' @import mgcv
+#'
+#' @export
+#'
+mergm <- function(formula, iter) {
+  #require("ergm")
+  #require("mgcv")
+
+  terms.vec <- attr(terms.formula(formula), "term.labels")
+
+  if (any(grepl("sociality", terms.vec))) {
+    stop(paste("Sociality must be excluded from the formula, since it will serve as an offset parameter."))
+  }
+
+  index <- 1:length(terms.vec)
+
+  formula.string <- paste(attr(terms(formula, keep.order = TRUE),
+                               "variables")[[2]], "~",
+                          paste(terms.vec[index], collapse = " + "),
+                          "+ offset(sociality(nodes = 1:nnodes))")
+
+  formula <- as.formula(formula.string)
+
+  formula.string2 <- paste(attr(terms(formula, keep.order = TRUE),
+                                "variables")[[2]], "~",
+                           paste(terms.vec[index], collapse = " + "))
+
+  formula2 <- as.formula(formula.string2)
+
+  net <- ergm::ergm.getnetwork(formula)
+  if(network::is.directed(net)) {
+    stop(paste("Only undirected networks can be handled at the moment."))
+  }
+
+  ## number of nodes in the network
+  nnodes <- network::get.network.attribute(net, "n")
+
+  ## number of dyads in the network
+  ndyads <- network::network.dyadcount(net)
+
+  ## extract the change statistics from the network model
+  dta.array <- ergm::ergmMPLE(formula2,
+                              output = "array",
+                              control = ergm::control.ergm(MPLE.max.dyad.types = ndyads*10))
+
+  ## number of the network statistics included in the model
+  ncoef <- length(dta.array$predictor[1,2,])
+
+  ## build matrix of the data
+  dta <- matrix(0, nrow = nnodes^2, ncol = 2 + ncoef)
+
+  idxx <- 1
+  for (tail in 1:(nnodes)) {
+    for (head in (nnodes):1) {
+      dta[idxx,] <- c(dta.array$response[tail, head],
+                      dta.array$predictor[tail, head, ],
+                      tail)
+      idxx <- idxx+1
+    }
+  }
+
+  ## save the matrix as data frame (long-format) and label it
+  dta <- data.frame(dta)
+  nm <- c("Y", names(dta.array$predictor[tail, head, ]),
+          "Sociality")
+  names(dta) <- nm
+  dta$Sociality <- as.factor(dta$Sociality)
+
+  ## fit a bam to the network data to get the node specific random effects
+  dta.bam <- mgcv::bam(Y ~ s(Sociality, bs = "re"),
+                       data = dta,
+                       family = "binomial",
+                       discrete = TRUE,
+                       nthreads = 5)
+  ref <- dta.bam$coefficients[-1]
+
+  ## save the results
+  results.coef.matrix <- matrix(nrow = iter, ncol = ncoef,
+                                dimnames = list(c(), c(names(dta.array$predictor[tail, head, ]))))
+
+  results.random.matrix <- matrix(nrow = length(unique(dta$Sociality)),
+                                  ncol = iter)
+
+  ## begin iteration switching between ergm and gam
+  for(i in 1:iter){
+
+    cat(paste("Iteration..."));cat("\n");cat(paste(i));cat("\n")
+
+    suppressMessages(model.ergm <- ergm(formula,
+                                        offset.coef = ref,
+                                        control = control.ergm(main.method = "Stepping",
+                                                               Step.maxit = 10,
+                                                               Step.MCMC.samplesize = 300,
+                                                               Step.gridsize = 100)))
+
+    # save the results in the matrix defined above
+    results.coef.matrix[i,] <- model.ergm$coef[1:ncoef]
+
+    ## take the results and build a formula to fit the gam
+    off <- rep("", times = length(ncoef))
+    for(z in 1:ncoef){
+      off[z] <- model.ergm$coef[z] * dta[1+z]
+      off[[z]] <- as.matrix(off[[z]])
+      s <- which(is.na(off[[z]]))
+      off[[z]][s,] <- 0
+      z <- z +1
+    }
+    off <- do.call(cbind, off)
+    off <- apply(off, 1, sum)
+
+    form.off <- noquote(paste("offset(off)", collapse = " + "))
+    form.bam <- paste("Y", form.off, sep = "~")
+    form.mixed.bam <- paste(form.bam, paste("s(Sociality, bs ='re')"), sep = " + ")
+    form.mixed.bam <- as.formula(form.mixed.bam)
+
+    model.bam <- mgcv::bam(form.mixed.bam,
+                           data = dta,
+                           family = "binomial",
+                           discrete = TRUE,
+                           nthreads = 5)
+
+    ref <- model.bam$coefficients[-1]
+    results.random.matrix[,i] <- ref
+
+    i <- i + 1
+
+  }
+
+  out = list(formula = formula,
+             nnodes = nnodes,
+             iterations = iter,
+             model = model.ergm,
+             coefs = results.coef.matrix[iter,1:ncoef],
+             coefs.all = results.coef.matrix[,1:ncoef],
+             mean.random.effects = mean(results.random.matrix[,iter]),
+             var.random.effects = var(results.random.matrix[,iter]),
+             random.effects = results.random.matrix[,iter],
+             random.effects.all = results.random.matrix)
+
+  return(out)
+
+}
